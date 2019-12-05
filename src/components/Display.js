6f5911c28dff9574820213547f27b5b874614c42
Dournbrood <tanner1220002@gmail.com>
2019-12-05 17:48:49 -0500

    display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,61 +1,17 @@
 import React, { useState } from "react";
 
-export const Display = () => {
-    const [scores, setScores] = useState({
-        balls: 0,
-        strikes: 0
-    });
-    //Our count function is going to live in Display. Things are bound to get ugly.
-    const gameCount = (type) => {
-
-        if (scores.strikes >= 3 || scores.balls >= 4) {
-            setScores({
-                balls: 0,
-                strikes: 0
-            });
-        }
-
-        else {
-            switch (type) {
-                case "strike":
-                    setScores({
-                        strikes: scores.strikes++
-                    });
-                    break;
-                case "ball":
-                    setScores({
-                        balls: scores.balls++
-                    })
-                    break;
-                case "foul": //This was FOUL to program... HA HA HA.                     Laugh.
-                    if (scores.strikes <= 1) {
-                        setScores({
-                            strikes: scores.strikes++
-                        })
-                    }
-                    else {
-                        //Do absolutely nothing.
-                        return;
-                    }
-                    break;
-                case "hit":
-                    setScores({
-                        balls: 0,
-                        strikes: 0
-                    });
-            }
-        }
-    }
-
+export const Display = (props) => {
     return (
         <div className="display">
-            <div className="balls">
-                <h1>Balls: {scores.balls}</h1>
+            <div className="balls" data-testid="balls-display">
+                <h1>Balls: {props.scores.balls}</h1>
             </div>
-            <div className="strikes">
-                <h1>Strikes: {scores.strikes}</h1>
+            <div className="strikes" data-testid="strikes-display">
+                <h1>Strikes: {props.scores.strikes}</h1>
             </div>
 
         </div>
     )
 }
+
+export default Display;
