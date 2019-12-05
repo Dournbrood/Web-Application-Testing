cc031337af728c2f44406cdb0b496c97dffb2eac
Dournbrood <tanner1220002@gmail.com>
2019-12-05 17:49:17 -0500

    nasty baseball functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Display from "./components/Display";
+import Dashboard from "./components/Dashboard";
 import './App.css';
 
 function App() {
+  const [scores, setScores] = useState({
+    balls: 0,
+    strikes: 0
+  });
+  //Our count function is going to live in Display. Things are bound to get ugly.
+  const gameCount = (type) => {
+    let { balls, strikes } = scores;
+
+    switch (type) {
+      case "strike":
+        strikes++;
+        break;
+      case "ball":
+        balls++;
+        break;
+      case "foul": //This was FOUL to program... HA HA HA.                     Laugh.
+        if (strikes <= 1) {
+          strikes++;
+        }
+        else {
+          //Do absolutely nothing.
+          return;
+        }
+        break;
+      case "hit":
+        strikes = 0;
+        balls = 0;
+    }
+    if (strikes >= 3 || balls >= 4) {
+      strikes = 0;
+      balls = 0;
+    }
+
+    setScores({ balls, strikes });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display scores={scores} />
+      <Dashboard gameCount={gameCount} />
     </div>
   );
 }
