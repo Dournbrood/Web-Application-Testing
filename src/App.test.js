5ea4e61bef4598656f6b5ae063a30496cc3e91ec
Dournbrood <tanner1220002@gmail.com>
2019-12-05 17:49:26 -0500

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+test('App renders', () => {
+  render(<App />);
+});
+
+test('All buttons render', () => {
+  const { getAllByTestId } = render(<App />);
+  getAllByTestId(/dash-button/i);
+});
+
+test('Balls render', () => {
+  const { getByTestId } = render(<App />);
+  getByTestId(/balls-display/i);
+});
+
+test('Strikes render', () => {
+  const { getByTestId } = render(<App />);
+  getByTestId(/strikes-display/i);
 });
